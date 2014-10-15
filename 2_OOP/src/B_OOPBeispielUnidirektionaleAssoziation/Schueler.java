7fde40c2b1b0d3735acb99eb6211403105a4c30a
hr <hammer.nikolai@googlemail.com>
2014-10-15 15:09:08 +0200

    files added

--- 2_OOP/src/B_OOPBeispielUnidirektionaleAssoziation/Schueler.java
+++ 2_OOP/src/B_OOPBeispielUnidirektionaleAssoziation/Schueler.java
@@ -1,7 +1,7 @@
 package B_OOPBeispielUnidirektionaleAssoziation;
 
 public class Schueler {
-    
+    //Objektattribute 
     String name;
     String vorname;
     
@@ -9,22 +9,60 @@ public class Schueler {
     Schulklasse dieSchulklasse;
     
     
-    
+    /**
+     * Konstruktor für Schülerobjekte.
+     * @param name
+     * @param vorname
+     * @param schulklasse
+     */
     public Schueler(String name, String vorname, Schulklasse schulklasse) {
-		
 		this.name = name;
 		this.vorname = vorname;
 		dieSchulklasse = schulklasse;
 	}
 
 
-	
-    //getter setter
+	//Getter und Setter
+    /**
+     * Versetzt einen Schüler in eine neue Klasse
+     * @param schulklasse
+     */
     public void setSchulklasse(Schulklasse schulklasse){
         dieSchulklasse = schulklasse;
     }
+    
+
+    public String getName() {
+		return name;
+	}
 
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getVorname() {
+		return vorname;
+	}
+
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+
+	public Schulklasse getDieSchulklasse() {
+		return dieSchulklasse;
+	}
+
+
+	/**
+     * Gibt alle Daten eines Schülers als Text zurück.
+     * Da die Informationen der Schulklasse mit ausgegeben werden,
+     * muss in der Klasse Schulklasse ebenfalls die toString Methode
+     * überschrieben werden.
+     */
 	@Override
 	public String toString() {
 		return "Schueler [name=" + name + ", vorname=" + vorname
--- 2_OOP/src/B_OOPBeispielUnidirektionaleAssoziation/StartKlasse.java
+++ 2_OOP/src/B_OOPBeispielUnidirektionaleAssoziation/StartKlasse.java
@@ -1,22 +1,32 @@
 package B_OOPBeispielUnidirektionaleAssoziation;
 
 public class StartKlasse {
+	
+	public static final int ANZAHL_KLASSEN = 5;
+	public static final int ANZAHLSCHUELER = 10;
 
   
     public static void main(String[] args) {
+    	Schulklasse klassen[] 	= new Schulklasse[ANZAHL_KLASSEN];
+    	Schueler 	schueler[] 	= new Schueler[ANZAHLSCHUELER];
+    	    	
         //Erzeugen der Objekte
-    	Schulklasse tg113 = new Schulklasse("TG11-3");
-        Schueler s1 = new Schueler("Mike","Mueller",tg113);
-        Schueler s2 = new Schueler("Max","Bohr",tg113);
-       
-        Schulklasse bktk = new Schulklasse("BKTK");
-        bktk.setBeschreibung("Berufskolleg für Technische Kommunikation");
+    	klassen[0] = new Schulklasse("TG11-1");
+    	klassen[1] = new Schulklasse("TG11-2");
+    	klassen[2] = new Schulklasse("TG11-3");
+    	klassen[3] = new Schulklasse("TG11-4");
+    	klassen[4] = new Schulklasse("BKTK");
+    	klassen[4].setBeschreibung("Berufskolleg für Technische Kommunikation");
+    	
+        schueler[0] = new Schueler("Mike","Mueller",klassen[0]);
+        schueler[1] = new Schueler("Max","Bohr",klassen[0]);
+        schueler[2] = new Schueler("Max","Bohr",klassen[4]);
         
-        Schueler s3 = new Schueler("Max","Bohr",bktk);
+        for (int i = 0; i < schueler.length; i++) {
+			System.out.println(schueler[i]);
+		}
         
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
+        
 
     }
 
