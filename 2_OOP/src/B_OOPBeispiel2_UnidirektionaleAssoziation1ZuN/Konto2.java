e543c1d0236b3ed2c3e99cd14db601bba7cc3b85
Nikolai Hammer <hammer.nikolai@googlemail.com>
2014-11-05 19:58:28 +0100

    Beispiel 2 zu Unidirektionaler Assoziation mit der Kardinalität N

---
+++ 2_OOP/src/B_OOPBeispiel2_UnidirektionaleAssoziation1ZuN/Konto2.java
@@ -0,0 +1,35 @@
+package B_OOPBeispiel2_UnidirektionaleAssoziation1ZuN;
+
+public class Konto2 {
+	
+	private int kontoNr;
+	private double saldo;
+	
+	public Konto2(int kontoNr, double saldo){
+		this.kontoNr = kontoNr;
+		this.saldo = saldo;
+	}
+
+	public int getKontoNr() {
+		return kontoNr;
+	}
+
+	public void setKontoNr(int kontoNr) {
+		this.kontoNr = kontoNr;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public String toString() {
+		return "Konto2 [kontoNr=" + kontoNr + ", saldo=" + saldo + "]";
+	}
+
+	
+}
---
+++ 2_OOP/src/B_OOPBeispiel2_UnidirektionaleAssoziation1ZuN/Kunde2.java
@@ -0,0 +1,64 @@
+package B_OOPBeispiel2_UnidirektionaleAssoziation1ZuN;
+
+import java.util.Vector;
+
+public class Kunde2 {
+	
+	private String name;
+	private String vorname;
+	
+	
+	
+	//Keklaration f?r die Aussoziation 1zuN
+	Vector<Konto2> dieKonten;
+	
+	
+	public Kunde2(String name, String vorname){
+		this.name = name;
+		this.vorname = vorname;
+		dieKonten = new Vector<Konto2>();
+	}
+	
+	
+	public Konto2 getKonto(int pos){
+		return dieKonten.elementAt(pos);
+	}
+	
+	public void addKonto(Konto2 konto){
+		dieKonten.add(konto);
+	}
+	
+	public void removeKonto(int pos){
+		dieKonten.remove(pos);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getVorname() {
+		return vorname;
+	}
+
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Kunde2 [name=" + name + ", vorname=" + vorname + ", dieKonten="
+				+ dieKonten + "]";
+	}
+	
+	
+
+}
