ab08ab325a3174adbaaf4ee5c1f917bb561389a2
hr <hammer.nikolai@googlemail.com>
2015-10-07 10:03:27 +0200

    Files added

--- 2_OOP/src/Proj1_DateiOeffnenUndSpeichern/Gui.java
+++ 2_OOP/src/Proj1_DateiOeffnenUndSpeichern/Gui.java
@@ -4,8 +4,26 @@ import java.io.File;
 
 import javax.swing.JFrame;
 
-public class Gui extends JFrame  {
+public class Gui extends JFrame implements Dateiverwaltung {
 	Steuerung dieSteuerung;
+
+	@Override
+	public File openFile(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveFile(File file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveAsFile(File file, String path) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 
--- 2_OOP/src/Proj1_DateiOeffnenUndSpeichern/Steuerung.java
+++ 2_OOP/src/Proj1_DateiOeffnenUndSpeichern/Steuerung.java
@@ -2,24 +2,7 @@ package Proj1_DateiOeffnenUndSpeichern;
 
 import java.io.File;
 
-public class Steuerung implements Dateiverwaltung{
-
-	@Override
-	public File openFile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void saveFile(File file) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void saveAsFile(File file, String path) {
-		// TODO Auto-generated method stub
-		
-	}
+public class Steuerung {
+	
 
 }
