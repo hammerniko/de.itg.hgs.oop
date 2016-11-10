7c7f07aee816a5650a6205f2773d1ae7af090383
nh <n.hammer@online.de>
2016-11-10 16:46:26 +0100

    Kommentare ergaenzt

--- 2_OOP/src/D2_VererbungUndGrafik/MeinPanel.java
+++ 2_OOP/src/D2_VererbungUndGrafik/MeinPanel.java
@@ -7,115 +7,128 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
-import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
+public class MeinPanel extends JPanel implements MouseMotionListener {
 
-
-public class MeinPanel extends JPanel implements MouseMotionListener{
-
+	/**
+	 * Konstanten
+	 */
+	private static final long serialVersionUID = 1L;
 	static final int FADENKREUZ_KREIS_KLEIN = 25;
 	static final int FADENKREUZ_KREIS_MITTEL = 50;
 	static final int FADENKREUZ_KREIS_GROSS = 75;
 	static final int FADENKREUZ_LINIE = 50;
-	
+
 	int h, b;
-	int x,y;	   //Mausposition
-	int xRel,yRel; //korrigierte Position abh. vom durchmesser des Fadenkreuzkreises 
-	
+	int x, y; 		// Mausposition
+	int xRel, yRel; // korrigierte Position abh. vom durchmesser des Fadenkreuzkreises
+
 	public MeinPanel() {
+		/**
+		 * FÃ¼gt dem Panelobjekt einen Listener hinzu, der auf Mausbewegungen hÃ¶rt.
+		 * Der Uebergabeparameter sorgt dafÃ¼r, das ein erzeugtes MouseMotion Event-Objekt
+		 * an die Methoden mouseMoved bzw. mouseDragged uebergeben wird.
+		 * Die Methoden sind vorhanden, da das MouseMotionListener Interface implementiert wurde.
+		 */
 		addMouseMotionListener(this);
 	}
-	
+
 	/**
-	 * Überschreiben der Standard Methode zum zeichnen der
-	 * Komponente.
-	 * 
-	 * Über das graphics-Objekt lassen sich die Standard
-	 * Methoden zum zeichnen von Linien, Rechteckecken, Kreises usw. aufrufen.
+	 * Ueberschreiben der Standard Methode zum zeichnen der Komponente.
+	 * Ueber das graphics-Objekt lassen sich die Standard Methoden zum zeichnen
+	 * von Linien, Rechteckecken, Kreises usw. aufrufen.
 	 */
-		
 	@Override
 	protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
+		/**Das JPanel soll erst mal gezeichnet werden wie ein Standard JPanel*/
 		super.paintComponent(g);
-		
+
+		/**Merken der aktuellen breite und hÃ¶he*/
 		b = getWidth();
 		h = getHeight();
+
+		/**
+		 * Fuer komplexere grafische darstellungen kann die Graphics2d Klasse
+		 * verwendet werden. Damit sind z.b: dickere Linien, Schatten usw.
+		 * moeglich.
+		 */
+		Graphics2D g2 = (Graphics2D) g;
 		
-		 /**
-		  * Für komplexere grafische darstellungen
-		  * kann die Graphics2d Klasse verwendet werden.
-		  * Damit sind z.b: dickere Linien, Schatten usw. möglich.
-		  */
-		 Graphics2D g2 = (Graphics2D) g;
-		 g2.setColor(Color.BLUE);
-	
+		/**Setzen der Farbe zum zeichnen */
+		g2.setColor(Color.BLUE);
+
 		setzeLinienstaerke(g2);
 		zeichneFadenkreuz(g2);
-		
+
 	}
 
 	private void setzeLinienstaerke(Graphics2D g2) {
-		BasicStroke linienstaerke
-         = new BasicStroke(2.0f, 
-        		           BasicStroke.CAP_SQUARE, 
-                           BasicStroke.JOIN_BEVEL);
-		 
+		BasicStroke linienstaerke = new BasicStroke(2.0f,BasicStroke.CAP_SQUARE, BasicStroke.JOIN_BEVEL);
 		g2.setStroke(linienstaerke);
-		
 	}
 
 	private void zeichneFadenkreuz(Graphics2D g2) {
-		xRel = x-(FADENKREUZ_KREIS_GROSS/2);
-		yRel = y-(FADENKREUZ_KREIS_GROSS/2);
+		xRel = x - (FADENKREUZ_KREIS_GROSS / 2);
+		yRel = y - (FADENKREUZ_KREIS_GROSS / 2);
 		g2.drawOval(xRel, yRel, FADENKREUZ_KREIS_GROSS, FADENKREUZ_KREIS_GROSS);
-		
-		xRel = x-(FADENKREUZ_KREIS_MITTEL/2);
-		yRel = y-(FADENKREUZ_KREIS_MITTEL/2);
-		g2.drawOval(xRel, yRel, FADENKREUZ_KREIS_MITTEL, FADENKREUZ_KREIS_MITTEL);
-		
-		xRel = x-(FADENKREUZ_KREIS_KLEIN/2);
-		yRel = y-(FADENKREUZ_KREIS_KLEIN/2);
+
+		xRel = x - (FADENKREUZ_KREIS_MITTEL / 2);
+		yRel = y - (FADENKREUZ_KREIS_MITTEL / 2);
+		g2.drawOval(xRel, yRel, FADENKREUZ_KREIS_MITTEL,
+				FADENKREUZ_KREIS_MITTEL);
+
+		xRel = x - (FADENKREUZ_KREIS_KLEIN / 2);
+		yRel = y - (FADENKREUZ_KREIS_KLEIN / 2);
 		g2.drawOval(xRel, yRel, FADENKREUZ_KREIS_KLEIN, FADENKREUZ_KREIS_KLEIN);
-		
-		
-		g2.drawLine(x, y-FADENKREUZ_LINIE, x, y+FADENKREUZ_LINIE);
-		g2.drawLine(x-FADENKREUZ_LINIE, y, x+FADENKREUZ_LINIE, y);
-		
+
+		g2.drawLine(x, y - FADENKREUZ_LINIE, x, y + FADENKREUZ_LINIE);
+		g2.drawLine(x - FADENKREUZ_LINIE, y, x + FADENKREUZ_LINIE, y);
+
 		hideCursor();
-		
+
 	}
 
+	/**
+	 * Versteckt den Mouse-Cursor 
+	 */
 	private void hideCursor() {
-		Image cursorImage = new BufferedImage(1,1,BufferedImage.TYPE_4BYTE_ABGR);
-		Point cursorPoint = new java.awt.Point(0,0);
-		Cursor cursor = java.awt.Toolkit.getDefaultToolkit().createCustomCursor(cursorImage,cursorPoint,"nocursor");
+		Image cursorImage = new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR);
+		Point cursorPoint = new java.awt.Point(0, 0);
+		Cursor cursor = java.awt.Toolkit.getDefaultToolkit().createCustomCursor(cursorImage, cursorPoint, "nocursor");
 		setCursor(cursor);
 	}
 
+	/**
+	 * Methoden mouseDragged,mouseMoved die das Interface MouseMotionListener erzwingt.
+	 */
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		//nichts machen
 	}
 
+	/**
+	 * Diese Methode wird aufgerufen, wenn die Maus Ã¼ber das Panel bewegt wird.
+	 * 
+	 */
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		System.out.println(e);
+		/**
+		 * aktuellen x-y Wert der Maus merken.
+		 */
 		x = e.getX();
 		y = e.getY();
+		
+		/**
+		 * ganzes Panel neu zeichen, wenn die Maus bewegt wird.
+		 * D.h. die Methode paintKomponent() wird aufgerufen.
+		 */
 		repaint();
-		
+
 	}
-	
-	
-	
-	
-		
-	
+
 }
