f22dd0e14592fb1574c7c517ba336f4885c3989b
nh <n.hammer@online.de>
2019-12-06 17:09:26 +0100

    umlaute

--- 2_OOP/src/F_Exceptions/WuerfelOutOfRangeException.java
+++ 2_OOP/src/F_Exceptions/WuerfelOutOfRangeException.java
@@ -3,6 +3,6 @@ package F_Exceptions;
 public class WuerfelOutOfRangeException  extends NumberFormatException{
 
 	public WuerfelOutOfRangeException() {
-		super("Der Wert für einen Würfel muss zwischen 1 und 6 liegen.");
+		super("Der Wert fuer einen Wuerfel muss zwischen 1 und 6 liegen.");
 	}
 }
