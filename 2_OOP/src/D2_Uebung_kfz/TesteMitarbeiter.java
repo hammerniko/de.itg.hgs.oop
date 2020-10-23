b4fe40482f2da1e46013628bf105024affcb9589
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-10-23 10:24:23 +0200

    modified

--- 2_OOP/src/D2_Uebung_kfz/TesteMitarbeiter.java
+++ 2_OOP/src/D2_Uebung_kfz/TesteMitarbeiter.java
@@ -3,18 +3,25 @@ package D2_Uebung_kfz;
 public class TesteMitarbeiter {
 
 	public static void main(String[] args) {
-		
+		 
+		 //Array für alle Mitarbeiter
 		 Mitarbeiter[] dieMitarbeiter = new Mitarbeiter[5];
+		 
+		 //Mitarbeiterobjekte erstellen und Daten setzen
 		 Sekretaerin moni = new Sekretaerin("Meier","Moni",15,30,152);
 		 KfzMechaniker dirk = new KfzMechaniker("Dirty", "Dirk", 20, 40);
 		 dirk.setUeberStundenAmWochenende(90);
 		 dirk.setUeberstunden(60);
 		
+		 //Mitarbeiterobjekte in Array speichern
 		 dieMitarbeiter[0] = moni;
 		 dieMitarbeiter[1] = dirk;
 		 
 		 
-		 //Schleife über alle Mitarbeiter
+		 /*		Schleife zur Ausgabe der Daten aller Mitarbeiter
+		  * 	Die toString() Methode wird implizit aufgerufen, wenn
+		  *     ein Objekt einer print-Anweisung übergeben wird. 
+		  */
 		 for (int i = 0; i < 5; i++) {
 			System.out.println(dieMitarbeiter[i]);
 		}
---
+++ 2_OOP/src/D2_Uebung_kfz/XYZ.java
@@ -0,0 +1,10 @@
+package D2_Uebung_kfz;
+
+public class XYZ extends Mitarbeiter {
+
+	public XYZ(String name, String vorname, double stundenlohn, double stundenProWoche) {
+		super(name, vorname, stundenlohn, stundenProWoche);
+		// TODO Auto-generated constructor stub
+	}
+
+}
