4164b3af6770cecf3c4530ab1676d7f2ed25a678
n.hammer <n.hammer@online.de>
2020-11-02 11:21:51 +0100

    Files Added

---
+++ 2_OOP/src/D03_VererbungBeiButton/Startklasse.java
@@ -0,0 +1,11 @@
+package D03_VererbungBeiButton;
+
+public class Startklasse {
+
+	public static void main(String[] args) {
+		GuiHauptprogramm gui = new GuiHauptprogramm();
+		gui.setVisible(true);
+		
+	}
+
+}
