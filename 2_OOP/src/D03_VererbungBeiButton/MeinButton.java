33765c9dd4a69a9d86c6f33b8999d4d499e9e627
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-11-02 12:59:55 +0100

    scaled IamgeIcons added

--- 2_OOP/src/D03_VererbungBeiButton/MeinButton.java
+++ 2_OOP/src/D03_VererbungBeiButton/MeinButton.java
@@ -1,8 +1,11 @@
 package D03_VererbungBeiButton;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class MeinButton extends JButton implements ActionListener{
@@ -11,7 +14,11 @@ public class MeinButton extends JButton implements ActionListener{
 	private static final int SPIELER_1  = -1;
 	private static final int SPIELER_2  = 1;
 	private static final int LEER  = 0;
+	private static final int ICON_SIZE=100;
 	
+	private ImageIcon icon;
+	private ImageIcon iconX;
+	private ImageIcon iconO;
     private int wert;
     
     public int getWert() {
@@ -23,35 +30,49 @@ public class MeinButton extends JButton implements ActionListener{
     }
 
     public MeinButton(){
+    	iconX = new ImageIcon("src//D03_VererbungBeiButton//x.jpg");
+    	iconO = new ImageIcon("src//D03_VererbungBeiButton//o.jpg");
+    	scaleIcon(iconX); 
+    	scaleIcon(iconO); 
+    	
         wert =SPIELER_1;
+        setBorder(null);
+        
         setZustand();
         addActionListener(this);
     }
 
+	private void scaleIcon(ImageIcon icon) {
+		icon.setImage(icon.getImage().getScaledInstance(ICON_SIZE,ICON_SIZE,Image.SCALE_DEFAULT));
+	}
+
     private void setZustand() {
         switch (wert) {
-        case SPIELER_1: setText("X"); break;
-        case SPIELER_2: setText("O");break;
-        case LEER: setText("");break;
-
+        case SPIELER_1: setIcon(iconX);  break;
+        case SPIELER_2: setIcon(iconO);break;
+        case LEER: setIcon(null);break;
 
         default:
             break;
         }
         
-        revalidate();
         
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	
         System.out.println("action");
         wert = wert * -1;
         setZustand();
         
+        revalidate();
         repaint();
+        getParent().revalidate();
         
     }
+    
+   
     
     
 
