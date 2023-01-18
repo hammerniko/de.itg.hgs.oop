2b8912e2b619acec9964e94b4d2baf1b04a323d8
hammerniko <hammernikolai@googlemail.com>
2023-01-18 17:13:24 +0100

    Interface Beispiel added

---
+++ 2_OOP/src/D5_interfaces/Auto.java
@@ -0,0 +1,9 @@
+package D5_interfaces;
+
+public interface Auto {
+	
+	public void bewegen(double wert);
+	public void bremsen(double wert);
+	
+
+}
---
+++ 2_OOP/src/D5_interfaces/MeinAuto.java
@@ -0,0 +1,50 @@
+package D5_interfaces;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MeinAuto implements Auto,MouseListener{
+
+	@Override
+	public void bewegen(double wert) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bremsen(double wert) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
